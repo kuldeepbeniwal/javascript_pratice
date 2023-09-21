d48b69d18c7d3a06c7c04f93f9f4fc07195e7604
Kuldeep Beniwal <kuldeepbeniwal220@gmail.com>
2023-09-21 23:18:12 +0530

    modified

--- 08_date.js
+++ 08_date.js
@@ -3,16 +3,16 @@ let myDate = new Date();
 // console.log(myDate.toString());
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleString());
-console.log (typeof myDate)
+console.log(typeof myDate);
 //let myCreatedDate=new Date(2023,0,23)
 //let myCreatedDate=new Date(2023,0,23,5,30)
-let myCreatedDate=new Date("2023-12-01")
+let myCreatedDate = new Date("2023-12-01");
 
 console.log(myCreatedDate.toLocaleString());
-let myTime=Date.now()
+let myTime = Date.now();
 console.log(myTime);
 
-let newDate = new Date()
+let newDate = new Date();
 console.log(newDate);
 console.log(newDate.getDate());
 console.log(newDate.getDay());
